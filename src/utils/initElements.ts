c5431b7c704565a1e015ac901d08f5b0c60d1a51
Daniel <schwartzray8@gmail.com>
2022-05-09 17:23:43 -0500

    typeof window !== 'undefined'

--- src/utils/initElements.ts
+++ src/utils/initElements.ts
@@ -23,11 +23,50 @@ export default function init_elements(
   const elements = [];
   let goingRight = false;
 
-  if (
-    window.innerWidth > window.innerHeight ||
-    !allowVertical ||
-    window === undefined
-  ) {
+  // !! Fix this terrible and ugly hack asap
+  if (window === undefined) {
+    milestones?.forEach((_, i) => {
+      if (i % 3 == 0) {
+        goingRight = !goingRight;
+      }
+
+      elements.push({
+        id: "horizontal-" + i,
+        sourcePosition: i % 3 == 2 ? "bottom" : goingRight ? "right" : "left",
+        targetPosition: i % 3 == 0 ? "top" : goingRight ? "left" : "right",
+        type: i == 0 ? "input" : null,
+        data:
+          milestones[i].length > milestoneSnippetSize
+            ? {
+                label: milestones[i].slice(0, 94) + "...",
+              }
+            : { label: milestones[i] },
+        position: goingRight
+          ? { x: 275 * (i % 3), y: Math.floor(i / 3) * 100 }
+          : { x: 275 * (2 - (i % 3)), y: Math.floor(i / 3) * 100 },
+        style: {
+          background: nodeProgressMap[milestoneProgress[i]],
+          color: "black",
+          border: "1px solid #222138",
+        },
+      });
+
+      if (i > 0) {
+        elements.push({
+          id: "e" + i,
+          arrowHeadType: "arrow",
+          source: "horizontal-" + (i - 1),
+          target: "horizontal-" + i,
+          animated: edgeProgressMap[milestoneProgress[i]],
+          sourcePosition: i % 3 == 2 ? "bottom" : goingRight ? "right" : "left",
+          targetPosition: i % 3 == 2 ? "top" : goingRight ? "left" : "right",
+        });
+      }
+    });
+    return elements;
+  }
+
+  if (window.innerWidth > window.innerHeight || !allowVertical) {
     milestones?.forEach((_, i) => {
       if (i % 3 == 0) {
         goingRight = !goingRight;
