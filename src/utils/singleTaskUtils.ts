d10f3ee8b045b5717de9345ae29c1311effe962f
Daniel <schwartzray8@gmail.com>
2022-06-11 10:01:03 -0400

    convertToSingleTask update

--- src/utils/singleTaskUtils.ts
+++ src/utils/singleTaskUtils.ts
@@ -14,7 +14,8 @@ export function convertToSingleTasks(
   } else if (responseTask.endOptions.date) {
     daysCount = numOccurrencesBetweenTwoDates(
       new Date(responseTask.startDate),
-      new Date(responseTask.endOptions.date)
+      new Date(responseTask.endOptions.date),
+      new Set([1, 3, 5])
     );
   } else {
     numTasks = responseTask.endOptions.repetitinos;
@@ -22,23 +23,58 @@ export function convertToSingleTasks(
   return daysCount;
 }
 
-function numOccurrencesBetweenTwoDates(start: Date, end: Date) {
+type EntryType = { idx: number; date: Date };
+
+function numOccurrencesBetweenTwoDates(
+  start: Date,
+  end: Date,
+  dayIdxs: Set<number>
+) {
   var dayCount = {
-    0: [{ idx: 0, date: Date() }],
-    1: [{ idx: 0, date: Date() }],
-    2: [{ idx: 0, date: Date() }],
-    3: [{ idx: 0, date: Date() }],
-    4: [{ idx: 0, date: Date() }],
-    5: [{ idx: 0, date: Date() }],
-    6: [{ idx: 0, date: Date() }],
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
   };
   for (var d = start; d <= end; d.setDate(d.getDate() + 1)) {
-    const currDay = dayCount[d.getDay()];
-    const prev = dayCount[d.getDay()][currDay.length - 1];
-    const newCount = prev["idx"] + 1;
-    const newDate = d;
-    const entry = { idx: newCount, date: newDate };
-    dayCount[d.getDay()].push(entry);
+    const dayIdx = d.getDay();
+    if (dayIdxs.has(dayIdx)) {
+      const currDay = dayCount[dayIdx];
+      const prev = dayCount[dayIdx][currDay.length - 1];
+      const newCount = prev ? prev["idx"] + 1 : 1;
+      const entry = { idx: newCount, date: d };
+      dayCount[dayIdx].push(entry);
+    }
   }
   return dayCount;
 }
+
+// function numOccurrencesBetweenTwoDates(start: Date, end: Date) {
+//   var dayCount = {
+//     0: [{ idx: 0, date: Date() }],
+//     1: [{ idx: 0, date: Date() }],
+//     2: [{ idx: 0, date: Date() }],
+//     3: [{ idx: 0, date: Date() }],
+//     4: [{ idx: 0, date: Date() }],
+//     5: [{ idx: 0, date: Date() }],
+//     6: [{ idx: 0, date: Date() }],
+//   };
+//   // !! Don't make it so it only works if day is in the set of chosen
+//   // !! Incase the person changes their mind about the days (or maybe do at first)
+
+//   // !! We are adding each of these as a row to singleTask data fram so
+//   // !! add the meta data: "completed", "notes", "expected completion date to the object"
+//   // !! Get all of this done tomorrow bitchh
+//   for (var d = start; d <= end; d.setDate(d.getDate() + 1)) {
+//     const currDay = dayCount[d.getDay()];
+//     const prev = dayCount[d.getDay()][currDay.length - 1];
+//     const newCount = prev["idx"] + 1;
+//     const newDate = d;
+//     const entry = { idx: newCount, date: newDate };
+//     dayCount[d.getDay()].push(entry);
+//   }
+//   return dayCount;
+// }
--- src/utils/test.js
+++ src/utils/test.js
@@ -1,22 +1,33 @@
-function numOccurrencesBetweenTwoDates(start, end) {
+// export type EntryType = { idx: number; date: Date };
+
+function numOccurrencesBetweenTwoDates(start, end, dayIdxs) {
   var dayCount = {
-    0: [{ idx: 0, date: Date() }],
-    1: [{ idx: 0, date: Date() }],
-    2: [{ idx: 0, date: Date() }],
-    3: [{ idx: 0, date: Date() }],
-    4: [{ idx: 0, date: Date() }],
-    5: [{ idx: 0, date: Date() }],
-    6: [{ idx: 0, date: Date() }],
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
   };
   for (var d = start; d <= end; d.setDate(d.getDate() + 1)) {
-    const currDay = dayCount[d.getDay()];
-    const prev = dayCount[d.getDay()][currDay.length - 1];
-    const newCount = prev["idx"] + 1;
-    const newDate = new Date(d);
-    const entry = { idx: newCount, date: newDate };
-    dayCount[d.getDay()].push(entry);
+    const dayIdx = d.getDay();
+    if (dayIdxs.has(dayIdx)) {
+      const currDay = dayCount[dayIdx];
+      const prev = dayCount[dayIdx][currDay.length - 1];
+      const newCount = prev ? prev["idx"] + 1 : 1;
+      const newDate = d;
+      const entry = { idx: newCount, date: d };
+      dayCount[dayIdx].push(entry);
+    }
   }
   return dayCount;
 }
 
-console.log(numOccurrencesBetweenTwoDates(new Date(), new Date(2023, 06, 10)));
+console.log(
+  numOccurrencesBetweenTwoDates(
+    new Date(2022, 6, 12),
+    new Date(2022, 6, 19),
+    new Set([1, 3, 5])
+  )
+);
