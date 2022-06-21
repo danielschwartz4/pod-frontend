346aca06a45de3134679abe46bd2c13cca851659
Daniel <schwartzray8@gmail.com>
2022-06-21 19:51:45 -0400

    format forgot password page

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -11,50 +11,72 @@ const ForgotPassword: React.FC<{}> = ({}) => {
   const [forgotPassword] = useForgotPasswordMutation();
 
   return (
-    <Flex justify={"center"} mt={20}>
-      <Flex pos={"absolute"} top={0} left={0} p={4}>
-        <NextLink href="/">
-          <Image cursor={"pointer"} h={70} w={200} src={firstLogo.src} alt="" />
-        </NextLink>
-      </Flex>
-      <Box w={"300px"}>
-        <Formik
-          initialValues={{ email: "" }}
-          onSubmit={async (values, { setErrors }) => {
-            await forgotPassword({
-              variables: values,
-            });
-            setComplete(true);
-          }}
+    <Flex
+      h={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={"gray.800"}
+      m={-2}
+    >
+      <Box w={"500px"}>
+        <Flex pos={"absolute"} top={0} left={0} p={4}>
+          <NextLink href="/">
+            <Image
+              cursor={"pointer"}
+              h={70}
+              w={200}
+              src={firstLogo.src}
+              alt=""
+            />
+          </NextLink>
+        </Flex>
+
+        <Box
+          rounded={"lg"}
+          bg={"gray.700"}
+          boxShadow={"lg"}
+          p={8}
+          m={4}
+          justifyContent={"center"}
         >
-          {({ isSubmitting }) =>
-            complete ? (
-              <Box>
-                if an account with that email exists, we sent you an email
-              </Box>
-            ) : (
-              <Form>
-                <Box mt={4}>
-                  <InputField
-                    name="email"
-                    label="Enter email to reset password"
-                    placeholder="email"
-                    type="email"
-                  />
+          <Formik
+            initialValues={{ email: "" }}
+            onSubmit={async (values, { setErrors }) => {
+              await forgotPassword({
+                variables: values,
+              });
+              setComplete(true);
+            }}
+          >
+            {({ isSubmitting }) =>
+              complete ? (
+                <Box>
+                  if an account with that email exists, we sent you an email
                 </Box>
-                <Button
-                  mt={4}
-                  isLoading={isSubmitting}
-                  colorScheme="teal"
-                  type="submit"
-                  cursor={isSubmitting ? "not-allowed" : "pointer"}
-                >
-                  forgot password
-                </Button>
-              </Form>
-            )
-          }
-        </Formik>
+              ) : (
+                <Form>
+                  <Box mt={4} mr={8}>
+                    <InputField
+                      name="email"
+                      label="Enter email to reset password"
+                      placeholder="email"
+                      type="email"
+                    />
+                  </Box>
+                  <Button
+                    mt={4}
+                    isLoading={isSubmitting}
+                    colorScheme="teal"
+                    type="submit"
+                    cursor={isSubmitting ? "not-allowed" : "pointer"}
+                  >
+                    forgot password
+                  </Button>
+                </Form>
+              )
+            }
+          </Formik>
+        </Box>
       </Box>
     </Flex>
   );
