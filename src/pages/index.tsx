22757b2f8068066321212efd7a2346023d79f597
Daniel <schwartzray8@gmail.com>
2022-07-11 20:39:45 -0400

    video tutorial on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Flex } from "@chakra-ui/react";
+import { AspectRatio, Box, Divider, Flex } from "@chakra-ui/react";
 import React from "react";
 import { Features } from "../components/Home/Features/Features";
 import Hero from "../components/Home/Hero";
@@ -17,9 +17,31 @@ const Index: React.FC<IndexProps> = ({}) => {
         </Box>
       </Flex>
       <Divider w={"80%"} transform={"translateY(40px)"} />
-      <Box mt={"4em"}>
-        <Samples />
-      </Box>
+      <Flex mt={"8em"}>
+        <Box mx={"auto"} display={["none", "block"]}>
+          <iframe
+            width="672"
+            height="378"
+            src="https://www.youtube.com/embed/k5YCmw6BNGQ"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Box>
+        <Box mx={"auto"} display={["block", "none"]}>
+          <iframe
+            width="336"
+            height="189"
+            src="https://www.youtube.com/embed/k5YCmw6BNGQ"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Box>
+      </Flex>
+      {/* <Samples /> */}
       <Divider w={"80%"} transform={"translateY(40px)"} />
     </Layout>
   );
