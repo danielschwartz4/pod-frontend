e15e7e77af171dce88fce4b2dfa4a5f60cecdc4d
Daniel <schwartzray8@gmail.com>
2022-07-11 20:55:53 -0400

    video tutorial on home page

---
+++ src/components/Home/SampleVideo.tsx
@@ -0,0 +1,45 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+interface SampleVideoProps {}
+
+const SampleVideo: React.FC<SampleVideoProps> = ({}) => {
+  return (
+    <Flex mt={"8em"} direction={"column"}>
+      <Flex>
+        <Text
+          fontFamily={"sans-serif"}
+          fontSize={24}
+          textColor={"gainsboro"}
+          mx={"auto"}
+        >
+          TUTORIAL
+        </Text>
+      </Flex>
+      <Box mt={8} mx={"auto"} display={["none", "block"]}>
+        <iframe
+          width="672"
+          height="378"
+          src="https://www.youtube.com/embed/k5YCmw6BNGQ"
+          title="Poddds tutorial"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </Box>
+      <Box mt={8} mx={"auto"} display={["block", "none"]}>
+        <iframe
+          width="336"
+          height="189"
+          src="https://www.youtube.com/embed/k5YCmw6BNGQ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </Box>
+    </Flex>
+  );
+};
+
+export default SampleVideo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { AspectRatio, Box, Divider, Flex } from "@chakra-ui/react";
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { Features } from "../components/Home/Features/Features";
 import Hero from "../components/Home/Hero";
-import { Samples } from "../components/Home/Samples";
+import SampleVideo from "../components/Home/SampleVideo";
 import { Layout } from "../components/Layout";
 
 interface IndexProps {}
@@ -17,30 +17,9 @@ const Index: React.FC<IndexProps> = ({}) => {
         </Box>
       </Flex>
       <Divider w={"80%"} transform={"translateY(40px)"} />
-      <Flex mt={"8em"}>
-        <Box mx={"auto"} display={["none", "block"]}>
-          <iframe
-            width="672"
-            height="378"
-            src="https://www.youtube.com/embed/k5YCmw6BNGQ"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Box>
-        <Box mx={"auto"} display={["block", "none"]}>
-          <iframe
-            width="336"
-            height="189"
-            src="https://www.youtube.com/embed/k5YCmw6BNGQ"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Box>
-      </Flex>
+      <Box>
+        <SampleVideo />
+      </Box>
       {/* <Samples /> */}
       <Divider w={"80%"} transform={"translateY(40px)"} />
     </Layout>
