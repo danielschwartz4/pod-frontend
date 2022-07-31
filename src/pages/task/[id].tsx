d39b460b110a73cf8832f1be164bcf3e4544e458
Daniel <schwartzray8@gmail.com>
2022-07-31 16:54:54 -0400

    updated feedback

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,31 +1,24 @@
 import { Box, Flex, Text, useToast } from "@chakra-ui/react";
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import { Layout } from "../../components/Layout";
-import { RecurringTaskProgress } from "../../components/MyRecurringTask/RecurringTaskProgress";
+import { MainDash } from "../../components/MyRecurringTask/MainDash";
 import { MyPod } from "../../components/MyTaskPod/MyPod";
+import Tour from "../../components/Tour";
 import {
-  SingleTask,
   useMeQuery,
   usePodQuery,
   usePodTasksQuery,
   useRecentPodSingleTasksQuery,
   useSingleTasksQuery,
 } from "../../generated/graphql";
-import { PageView } from "../../libs/tracking";
-import { NotePopup, randNotesSplurge } from "../../utils/randNotesSplurge";
 import { useGetTaskFromUrl } from "../../utils/useGetTaskFromUrl";
 import { useIsAuth } from "../../utils/usIsAuth";
 
 interface TaskHomeProps {}
 
 const TaskHome: React.FC<TaskHomeProps> = ({}) => {
-  useEffect(() => PageView(), []);
+  // useEffect(() => PageView(), []);
   useIsAuth();
-
-  const [changeTab, useChangeTab] = useState<string>("task");
-  const [keepMounted, setKeepMounted] = useState(true);
-
-  const TEMP_BOOL = true;
 
   const { data: meData } = useMeQuery({});
 
@@ -37,7 +30,7 @@ const TaskHome: React.FC<TaskHomeProps> = ({}) => {
     refetch: refetchTask,
   } = useGetTaskFromUrl();
 
-  const { data: podData } = usePodQuery({
+  const { data: podData, loading: podDataLoading } = usePodQuery({
     variables: { podId: myTaskData?.recurringTask?.task?.podId },
   });
 
@@ -61,67 +54,58 @@ const TaskHome: React.FC<TaskHomeProps> = ({}) => {
 
   const { data: recentPodSingleTasksData } = useRecentPodSingleTasksQuery({
     variables: {
-      taskIds: singleTasksData?.singleTasks?.singleTasks.map((task) => task.id),
+      // taskIds: singleTasksData?.singleTasks?.singleTasks.map((task) => task.id),
+      taskIds: tasksData?.podTasks?.map((task) => task.id),
     },
   });
-
-  async function notesToastHandler(popup: NotePopup) {
-    toast({
-      title: popup.title,
-      description: popup.description,
-      position: "bottom",
-      status: "info",
-      duration: null,
-      isClosable: true,
-    });
-  }
-
-  useMemo(() => {
-    if (
-      tasksData?.podTasks &&
-      tasksData?.podTasks?.length > 1 &&
-      !tasksDataLoading
-    ) {
-      const popups = randNotesSplurge(
-        recentPodSingleTasksData?.recentPodSingleTasks
-          ?.singleTasks as SingleTask[],
-        tasksData?.podTasks?.length
-      );
-      popups?.forEach(async (popup) => {
-        notesToastHandler(popup);
-      });
-    }
-  }, [recentPodSingleTasksData]);
+  console.log(recentPodSingleTasksData);
 
   return (
-    <Layout withHelpPopover={true}>
-      <Box minH={"100vh"} h={"100%"} mt={{ base: 0, sm: 16 }}>
-        <Flex>
-          {!taskDataLoading && !singleTasksDataLoading ? (
-            <RecurringTaskProgress
-              singleTasksData={singleTasksData}
-              myTaskData={myTaskData}
-              refetchSingleTasks={refetchSingleTasks}
-            />
-          ) : (
-            <Text>Loading...</Text>
-          )}
-        </Flex>
+    <>
+      <Tour />
+      <Layout withHelpPopover={true}>
+        <Box minH={"100vh"} h={"100%"} mt={{ base: 0, sm: 16 }}>
+          <Box minH={"400px"}>
+            {!taskDataLoading && !singleTasksDataLoading ? (
+              <MainDash
+                recentPodSingleTasksData={recentPodSingleTasksData}
+                singleTasksData={singleTasksData}
+                myTaskData={myTaskData}
+                refetchSingleTasks={refetchSingleTasks}
+              />
+            ) : (
+              <Flex mt={16}>
+                <Text fontSize={30} mx={"auto"} textColor={"gainsboro"}>
+                  Dashboard loading...
+                </Text>
+              </Flex>
+            )}
+          </Box>
 
-        <Box mt={8}>
-          <MyPod
-            podData={podData}
-            meData={meData}
-            tasksData={tasksData}
-            taskDataLoading={taskDataLoading}
-            tasksDataLoading={tasksDataLoading}
-            myTaskData={myTaskData}
-            refetchTask={refetchTask}
-            refetchTasks={refetchTasks}
-          />
+          <Box mt={16}>
+            {!podDataLoading && !taskDataLoading ? (
+              <MyPod
+                podData={podData}
+                meData={meData}
+                tasksData={tasksData}
+                taskDataLoading={taskDataLoading}
+                tasksDataLoading={tasksDataLoading}
+                myTaskData={myTaskData}
+                refetchTask={refetchTask}
+                refetchTasks={refetchTasks}
+                recentPodSingleTasksData={recentPodSingleTasksData}
+              />
+            ) : (
+              <Flex mt={16}>
+                <Text fontSize={30} mx={"auto"} textColor={"gainsboro"}>
+                  Pods loading...
+                </Text>
+              </Flex>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
